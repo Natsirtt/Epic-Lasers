20461f3a2ead37ff1d769282f9a2929ab006991e
Natsirtt <natsirtt@gmail.com>
2013-03-30 17:35:20 +0100

    Adding the header of the loadings module

---
+++ loadings.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   loadings.h
+ * Author: natsirtt
+ *
+ * Created on 30 mars 2013, 17:34
+ */
+
+#ifndef LOADINGS_H
+#define	LOADINGS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* LOADINGS_H */
+
